3054e3597fafc647262e8079de898a1d61b4b8d5
이상현 <tkdgus3769@naver.com>
2024-10-17 12:19:53 +0900

    이상현 숙제 제출

---
+++ Java_Practice2/src/ex_241017/homework/Lsh0708.java
@@ -0,0 +1,10 @@
+package ex_241017.homework;
+
+public class Lsh0708 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이상현 숙제 제출");
+	}
+
+}
