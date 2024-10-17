4f23e0148643bdfb54c7ca217f6c32614b319ecf
Duhyeon Kang <rkdengus1234@naver.com>
2024-10-17 13:58:52 +0900

    강두현 숙제제출2

--- Java_Practice2/src/ex_241017/homework/Kdh1018.java
+++ Java_Practice2/src/ex_241017/homework/Kdh1018.java
@@ -5,6 +5,7 @@ public class Kdh1018 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("강두현 숙제 제출");
+		System.out.println("강두현 숙제 제출2");
 	}
 
 }
