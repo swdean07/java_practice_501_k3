b468eda7264f1765a834722e9be4cba04dd9ddf2
subin806 <chosb0806@naver.com>
2024-10-17 12:19:57 +0900

    조수빈 숙제 제출

---
+++ Java_Practice2/src/ex_241017/homework/Csb0806.java
@@ -0,0 +1,10 @@
+package ex_241017.homework;
+
+public class Csb0806 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("조수빈 숙제 제출");
+	}
+
+}
