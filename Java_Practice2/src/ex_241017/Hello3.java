1c8c4b0e400c147c7e32d230ef50b71e88ff80f8
이상용 <lsy3709@naver.com>
2024-10-17 11:19:03 +0900

    깃테스트2

---
+++ Java_Practice2/src/ex_241017/Hello3.java
@@ -0,0 +1,20 @@
+package ex_241017;
+
+public class Hello3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi~~");
+		System.out.println("hi2~~");
+		
+		// 1 sysout 단축키 : 참고 자료
+		// https://sycareer.tistory.com/entry/%EC%9D%B4%ED%81%B4%EB%A6%BD%EC%8A%A4-%EC%9E%90%EB%B0%94-sysout-%EC%9E%90%EB%8F%99%EC%99%84%EC%84%B1-%EC%95%88%EB%90%A0-%EB%95%8C-Systemoutprintln-%EC%9E%90%EB%8F%99-%EC%99%84%EC%84%B1
+
+		// 2 자동 완성 설정 참고 자료
+		// https://velog.io/@zz1996zz/Eclipse%EC%9D%B4%ED%81%B4%EB%A6%BD%EC%8A%A4-%EC%9E%90%EB%8F%99%EC%99%84%EC%84%B1-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0
+
+		// 3 자동 줄 맞춤
+		// 단축키 : ctrl + shift + f
+	}
+
+}
