940d10165c323e343cc381a71f74e65f1f8a6753
이상현 <tkdgus3769@naver.com>
2024-10-18 09:41:27 +0900

    1018 이상현 깃테스트

--- Java_Practice2/src/ex_241018_ch1/homework/Lsh0708.java
+++ Java_Practice2/src/ex_241018_ch1/homework/Lsh0708.java
@@ -4,7 +4,7 @@ public class Lsh0708 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("깃테스트2 PR보내기");
+		System.out.println("깃테스트2 PR보내기!!");
 	}
 
 }
