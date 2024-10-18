10137f66cc1dcd8904b36b4bc183302388fe15a7
Yuu-Yur <gkcjdqls@gmail.com>
2024-10-18 09:24:43 +0900

    하청빈 1018 1교시 제출

---
+++ Java_Practice2/src/ex_241018_ch1/homework/Hcb9404.java
@@ -0,0 +1,10 @@
+package ex_241018_ch1.homework;
+
+public class Hcb9404 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1018 1교시 깃 테스트 2");
+	}
+
+}
