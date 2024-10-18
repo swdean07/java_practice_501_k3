408e10875d406af838dcf1812c12664de313e652
leesanghwa1997 <zlqlcm1997@gmail.com>
2024-10-18 12:46:21 +0900

    파일삭제, 회원가입 코드작성

--- Java_Practice2/src/ex_241018_ch1/homework/Lsh1208.java
+++ Java_Practice2/src/ex_241018_ch1/homework/Lsh1208.java
@@ -2,9 +2,13 @@ package ex_241018_ch1.homework;
 
 public final class Lsh1208 {
 
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("1교시 숙제");
+		int result = sum(1,2);
+		System.out.println("클래스 메서드(static) 이용해보기, result:"+ result);
 	}
 
 }
