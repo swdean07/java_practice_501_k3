47da1e184fe19e9450d604519759099e64c9ea77
전효정 <kyn04221@gmail.com>
2024-10-18 09:49:57 +0900

    깃테스트2 전효정

---
+++ Java_Practice2/src/ex_241018_ch1/homework/HJ_0406.java
@@ -0,0 +1,10 @@
+package ex_241018_ch1.homework;
+
+public class HJ_0406 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("전효정, 깃테스트 ");
+	}
+
+}
