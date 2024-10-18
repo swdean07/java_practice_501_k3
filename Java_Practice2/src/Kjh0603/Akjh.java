9a35ff0614d7e0662c052b5a8ab4980e6f5f5a24
kimjaehan06 <onlykjh06@gmail.com>
2024-10-18 09:29:10 +0900

    김재한

---
+++ Java_Practice2/src/Kjh0603/Akjh.java
@@ -0,0 +1,10 @@
+package Kjh0603;
+
+public class Akjh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("김재한");
+	}
+
+}
--- Java_Practice2/src/ex_241017/Homewark/Kjh0313.java
+++
@@ -1,6 +0,0 @@
-package ex_241017.Homewark;
-
-public class Kjh0313 {
-	// TODO Auto-generated method stub
-			System.out.println("김재한");
-}
