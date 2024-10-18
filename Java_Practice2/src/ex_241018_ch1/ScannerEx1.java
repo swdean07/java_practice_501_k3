8b31eb95d3a49d6832e66c04de18ff7433a5eb2c
이상용 <lsy3709@naver.com>
2024-10-18 12:24:41 +0900

    Scanner 테스트 및 미니 실습 해보기.

---
+++ Java_Practice2/src/ex_241018_ch1/ScannerEx1.java
@@ -0,0 +1,30 @@
+package ex_241018_ch1;
+
+import java.util.Scanner;
+
+public class ScannerEx1 {
+
+	public static void main(String[] args) {
+		//System.in, 키보드로 입력된 내용이 바이트 코드로 변환 해주는 역할. 
+		// Scanner 이용해서, 우리가 사용하는 문자열, 정수 형태로 이용. 
+		// 자바에 기본으로 탑재된 기능. 다른 패키지에 있으면, 반드시 import 후 사용 가능. 
+		// IDE , 자동으로 임포트 , 패키지 명이 겹치면, 선택만 해주기. 
+		System.out.println("Scanner를 이용한, 콘솔에 입력한 문자열, 숫자 입력 받기.");
+		
+		// Scanner 객체, 인스턴스 만들기. 
+		// 정의, 
+		// 클래스타입  참조형변수 = new  생성자();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("오늘 먹고 싶은 점심 메뉴입력해주세요 : ");
+		String lunchMenu = scanner.next();
+		System.out.println("먹고 싶은 메뉴 : " + lunchMenu);
+		
+		System.out.println("주말에 먹고싶은 메뉴 입력하기 : ");
+		String weekendMenu = scanner.next();
+		System.out.println("주말에 먹고 싶은 메뉴 : " + weekendMenu);
+		
+		
+
+	}
+
+}
