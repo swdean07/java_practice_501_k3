478f5bca603c4d742068658a02bd4c8ca3de272a
kimdh32022 <kimdh32022@naver.com>
2024-10-18 10:12:12 +0900

    1018/1교시 다시 제출

---
+++ Java_Practice2/src/ex_241018_ch1/homework/Dohyeon0222.java
@@ -0,0 +1,10 @@
+package ex_241018_ch1.homework;
+
+public class Dohyeon0222 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요 김도현입니다. ");
+	}
+
+}
