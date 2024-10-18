dfbcd52580ecd3b0364250ecfd1405a0519feb77
Duhyeon Kang <rkdengus1234@naver.com>
2024-10-18 09:35:28 +0900

    강두현숙제제출1018

--- Java_Practice2/src/ex_241018_ch1/homework/Kdh1018.java
+++ Java_Practice2/src/ex_241018_ch1/homework/Kdh1018.java
@@ -5,6 +5,7 @@ public class Kdh1018 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("깃테스트2,강두현. ");
+		System.out.println("깃테스트2,강두현. ");
 	}
 
 }
