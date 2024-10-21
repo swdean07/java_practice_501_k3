4b732ce63f7101b61fa5f24ce6b48edd7cefa2af
kimjaehan06 <onlykjh06@gmail.com>
2024-10-21 12:23:05 +0900

    김재한 241021 3교시 과제 제출

---
+++ Java_Practice2/src/ex_241021_ch3/homework/Kjh0313.java
@@ -0,0 +1,21 @@
+package ex_241021_ch3.homework;
+
+import java.util.Scanner;
+
+public class Kjh0313 {
+	public static void main(String[] args) {
+		String[] menu = new String[3];
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("오늘 먹고 싶은 점심 메뉴 3개 작성해보기 : ");
+		for (int i = 0; i < menu.length; i++) {
+			System.out.print("메뉴 " + (i + 1) + " : ");
+			menu[i] = scanner.nextLine();
+		}
+		System.out.println("");
+		System.out.println("오늘 먹고 싶은 메뉴 :");
+		for (String m : menu) {
+			System.out.println(m);
+		}
+		scanner.close();
+	}
+}
