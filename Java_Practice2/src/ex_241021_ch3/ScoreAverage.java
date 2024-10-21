d64a3b065ea6b2e3cf3d233c8c1e3af64aec07dd
이상용 <lsy3709@naver.com>
2024-10-21 14:20:18 +0900

    5교시, 이중 배열 각 원소 표기 후 출력 해보기

--- Java_Practice2/src/ex_241021_ch3/ScoreAverage.java
+++ Java_Practice2/src/ex_241021_ch3/ScoreAverage.java
@@ -2,16 +2,36 @@ package ex_241021_ch3;
 
 public class ScoreAverage {
 	public static void main(String[] args) {
+		//1
+		// 2중 배열 정의 및 값을 할당 초기화 같이 실행. 
+		// double score[][] = new double[4][2]
 		double score[][] = { { 3.3, 3.4 }, // 1학년1, 2학기평점
 				{ 3.5, 3.6 }, // 2학년1, 2학기평점
 				{ 3.7, 4.0 }, // 3학년1, 2학기평점
 				{ 4.1, 4.2 } }; // 4학년1, 2학기평점
+		// 임시 변수, 합계
+		//2
 		double sum = 0;
-		for (int year = 0; year < score.length; year++) // 각학년별로반복
-			for (int term = 0; term < score[year].length; term++) // 각학년의학기별로반복
-				sum += score[year][term]; // 전체평점합
+		// 이중 for 문 
+		
+		//3
+		 // 각학년별로반복
+		for (int year = 0; year < score.length; year++) {
+			// 3-1
+			// 각학년의학기별로반복
+			for (int term = 0; term < score[year].length; term++) { 
+				// 3-1-1
+				// 전체평점합
+				sum += score[year][term];
+				System.out.println("score["+year +"]["+term+"] :" + score[year][term]);
+			}
+		}
+		
+		//4
 		int n = score.length; // 배열의행개수, 4
+		//5
 		int m = score[0].length; // 배열의열개수, 2
+		//6
 		System.out.println("4년전체평점평균은" + sum / (n * m));
 	}
 }
