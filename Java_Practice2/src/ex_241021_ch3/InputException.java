4d6f2a7ea1d74ad2de8a136bb89495c8594fc677
이상용 <lsy3709@naver.com>
2024-10-21 15:26:45 +0900

    6교시, 예외처리 타입 일치 여부 확인2

--- Java_Practice2/src/ex_241021_ch3/InputException.java
+++ Java_Practice2/src/ex_241021_ch3/InputException.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 
 public class InputException {
 	public static void main(String[] args) {
+		//1
 		Scanner scanner = new Scanner(System.in);
+		//2
 		System.out.println("정수 3개를 입력하세요");
+		//3
 		int sum = 0, n = 0;
+		//4
 		for (int i = 0; i < 3; i++) {
+			// 1
 			System.out.print(i + ">>");
+			// 2
 			try {
 				n = scanner.nextInt(); // 정수 입력
 			} catch (InputMismatchException e) {
@@ -20,10 +26,12 @@ public class InputException {
 				i--; // 인덱스가 증가하지 않도록 미리 감소
 				continue; // 다음 루프
 			}
+			//3
 			sum += n; // 합하기
 		}
-
+		//5
 		System.out.println("합은 " + sum);
+		//6
 		scanner.close();
 	}
 }
