5d03c7351ce68a242324ec1bd48bb78bbc9129ee
이상용 <lsy3709@naver.com>
2024-10-22 14:03:35 +0900

    5교시, 클래스, 배열 참조형 확인중

--- Java_Practice2/src/ex_241022_ch4_2/Circle.java
+++ Java_Practice2/src/ex_241022_ch4_2/Circle.java
@@ -2,11 +2,25 @@ package ex_241022_ch4_2;
 
 public class Circle {
 	int radius; // 원의 반지름 필드
+	
 	String name; // 원의 이름을 필드
 
 	public double getArea() { // 멤버 메소드
 		return 3.14 * radius * radius;
 	}
+	
+	// 매개변수가 1개인 생성자 
+	public Circle(int radius) {
+		super();
+		this.radius = radius;
+	}
+	
+	// 직접 생성자를 만들면, 기본 생성자를 만들어주지 않는다, 
+	// 그래서, 직접 정의한다.
+	public Circle() {
+		
+	}
+	
 
 	public static void main(String[] args) {
 		Circle pizza;
