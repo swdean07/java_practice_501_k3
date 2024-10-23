e106c06121427122f94987a6735d0cdd23919504
이상용 <lsy3709@naver.com>
2024-10-23 11:47:12 +0900

    3교시, 클래스, 상속, 타입 확인

---
+++ Java_Practice2/src/ex_241023_ch5/ColorPoint.java
@@ -0,0 +1,40 @@
+package ex_241023_ch5;
+
+class Point {
+	private int x, y; // 한 점을 구성하는 x, y 좌표
+
+	// 생성자 없네요. 기본 생성자가 생략된 상태
+	
+//	Point(){
+	// super();
+//}
+	
+	public void set(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void showPoint() { // 점의 좌표 출력
+		System.out.println("(" + x + "," + y + ")");
+	}
+}
+
+// Point를 상속받은 ColorPoint 선언 
+class ColorPoint extends Point {
+	private String color; // 점의 색
+
+	// 기본 생성자를 생략. 
+	ColorPoint(){
+	 super();
+}
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void showColorPoint() { // 컬러 점의 좌표 출력
+		System.out.print(color);
+		showPoint(); // Point의 showPoint() 호출
+	}
+}
+
+
