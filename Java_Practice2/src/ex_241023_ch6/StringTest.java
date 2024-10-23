0978fb9a60d841c2f1752510d292ce802a092ff1
이상용 <lsy3709@naver.com>
2024-10-23 15:07:27 +0900

    6교시, StringBuffer 클래스, 가변 수정 가능, 매번 새로운 메모리 사용안함, 기존 메모리 사용함.

--- Java_Practice2/src/ex_241023_ch6/StringTest.java
+++ Java_Practice2/src/ex_241023_ch6/StringTest.java
@@ -18,7 +18,30 @@ public class StringTest {
 		System.out.println("e heap의 임시 주솟값 : " + System.identityHashCode(e));
 		System.out.println("f : Hello heap의 임시 주솟값 : " + System.identityHashCode(f));
 		
+		// 리터럴, 힙으로 만든 문자열 수정 불가능, 
+		// 수정 불가능 -> 기존에 만들었던 저장공간을 사용안하고, 또다른 메모리 공간 사용을함.
+		// String 변경 할 때마다, 메모리를 추가로 사용을한다. -> 낭비가 됨. 
+		// 개선책. StringBuffer 
+		
+		String result = a + "1";
+		System.out.println("result : a + \"1\" ,리터럴의 임시 주솟값 : " + System.identityHashCode(result));
+		
+		String result2 = d.concat("1");
+		System.out.println("result2 : d.concat(\"1\") ,리터럴의 임시 주솟값 : " + System.identityHashCode(result2));
 
+		
+		// 개선한 클래스 , StringBuffer 
+		StringBuffer sb = new StringBuffer("hello");
+		System.out.println("sb : Hello heap의 임시 주솟값 : " + System.identityHashCode(sb));
+		sb.append("1");
+		System.out.println(sb.toString());
+		System.out.println("sb : Hello heap의 임시 주솟값 : " + System.identityHashCode(sb));
+		
 	}
 
 }
+
+
+
+
+
