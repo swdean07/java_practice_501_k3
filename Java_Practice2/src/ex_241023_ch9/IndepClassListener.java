03960045483058260dbe8c569b58f7e683cb126e
이상용 <lsy3709@naver.com>
2024-10-24 16:22:52 +0900

    7교시, 이벤트 처리 확인

--- Java_Practice2/src/ex_241023_ch9/IndepClassListener.java
+++ Java_Practice2/src/ex_241023_ch9/IndepClassListener.java
@@ -13,10 +13,15 @@ public class IndepClassListener extends JFrame {
 	public IndepClassListener() {
 		setTitle("Action 이벤트 리스너 예제");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// 해당 최상위 컨테이너(윈도우 창)에서의 사용할 패널을 가리킴. 
 		Container c = getContentPane();
+		// flex 와 비슷., 나란히 배치. 
 		c.setLayout(new FlowLayout());
+		// UI 버튼 생성
 		JButton btn = new JButton("Action");
+		// 버튼에 이벤트 리스너를 추가하는 부분. 
 		btn.addActionListener(new MyActionListener());
+		// UI 컨테이너 패널에 버튼 추가. 
 		c.add(btn);
 		setSize(250, 120);
 		setVisible(true);
@@ -31,11 +36,24 @@ public class IndepClassListener extends JFrame {
 
 // 독립된 클래스로 이벤트 리스너를 작성한다.
 class MyActionListener implements ActionListener {
+
+	@Override
 	public void actionPerformed(ActionEvent e) {
+		// 버튼 클릭(화면에 클릭 대상이 보이지만, 컴퓨터 입장에서는 몰라요. 
+		// 무엇을 클릭 하는지 대상체 ?
 		JButton b = (JButton) e.getSource();
+		// 버튼의 문자열의 내용으로 현재 구분 중. 
 		if (b.getText().equals("Action"))
 			b.setText("액션");
 		else
 			b.setText("Action");
 	}
+
 }
+
+
+
+
+
+
+
