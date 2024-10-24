4236ed701433106a0c1e218d4f082684ac827c8a
이상용 <lsy3709@naver.com>
2024-10-24 15:19:36 +0900

    6교시, 컨테이너 컴포넌트 붙이기 예제

---
+++ Java_Practice2/src/ex_241023_ch8/ContentPaneEx.java
@@ -0,0 +1,28 @@
+package ex_241023_ch8;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class ContentPaneEx extends JFrame {
+	public ContentPaneEx() { 
+		setTitle("ContentPane과 JFrame 예제"); // 프레임의 타이틀 달기 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+		Container contentPane = getContentPane(); // 컨텐트팬 알아내기 
+		contentPane.setBackground(Color.ORANGE); // 오렌지색 배경 설정 
+		contentPane.setLayout(new FlowLayout()); // 컨텐트팬에 FlowLayout  
+		
+		contentPane.add(new JButton("OK")); // OK 버튼 달기 
+		contentPane.add(new JButton("Cancel")); // Cancel 버튼 달기 
+		contentPane.add(new JButton("Ignore")); // Ignore 버튼 달기 
+		setSize(300, 150); // 프레임 크기 300x150 설정 
+		setVisible(true); // 화면에 프레임 출력 
+	}
+
+	public static void main(String[] args) {
+		new ContentPaneEx();
+	}
+}
