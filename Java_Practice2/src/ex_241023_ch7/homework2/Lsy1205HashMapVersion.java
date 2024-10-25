6ea267131ec0f95923b5e3b0232f185487a2528f
이상용 <lsy3709@naver.com>
2024-10-25 09:35:34 +0900

    1교시, 검색 기능 확인.

--- Java_Practice2/src/ex_241023_ch7/homework2/Lsy1205HashMapVersion.java
+++ Java_Practice2/src/ex_241023_ch7/homework2/Lsy1205HashMapVersion.java
@@ -11,7 +11,7 @@ import util.UtilLsy1205Test;
 
 public class Lsy1205HashMapVersion {
 	// 함수의 정의 부분, 소괄호 안에 있는 매개변수(받는 변수) 정의.
-	public static void contactProgram(Scanner scanner, HashMap<String,PersonLsy1205Test> persons) {
+	public static void contactProgram(Scanner scanner, HashMap<String, PersonLsy1205Test> persons) {
 		// 반복문으로 콘솔 상에서, 메인 메뉴가 계속 출력 되는 기능, UI
 		while (true) {
 			System.out.println("===================================================");
@@ -43,39 +43,40 @@ public class Lsy1205HashMapVersion {
 
 			} else if (check.equals("2")) {
 				System.out.println("출력 기능을 구현 할 예정");
-				// hashMap -> iteratorable , 가상 테이블의 0행 부터 각행을 조회해서 구현 
-				Set<String> keys = persons.keySet(); // 모든 키를 Set 컬렉션에 받아옴 
-				  Iterator<String> it = keys.iterator(); // Set에 접근하는 Iterator 리턴 
-				  while(it.hasNext()) { 
-				   String key = it.next(); // 키 
-				   PersonLsy1205Test value = persons.get(key); // 값 
-				   System.out.println("(이름: " + key + " , 연락처 : "
-				   + value.getContact() + ", 등록날짜 : "+ value.getRegDate()+" ) "); 
-				  } 
+				// hashMap -> iteratorable , 가상 테이블의 0행 부터 각행을 조회해서 구현
+				Set<String> keys = persons.keySet(); // 모든 키를 Set 컬렉션에 받아옴
+				Iterator<String> it = keys.iterator(); // Set에 접근하는 Iterator 리턴
+				while (it.hasNext()) {
+					String key = it.next(); // 키
+					PersonLsy1205Test value = persons.get(key); // 값
+					System.out.println("(이름: " + key + " , 연락처 : " + value.getContact() + ", 등록날짜 : "
+							+ value.getRegDate() + " ) ");
+				}
 			} else if (check.equals("3")) {
 				System.out.println("검색 기능을 구현 할 예정");
 				System.out.println("검색할 이름:>> ");
+				// 검색어
 				String search = scanner.next();
+				// keys , HashMap에 등록된 모든 키 정보
+				Set<String> keys = persons.keySet(); // 모든 키를 Set 컬렉션에 받아옴
+				// 반복이 구현 가능한 형태로 변경 -> 가상테이블 표현. 0행에 대기중. 커서 같이.
+				Iterator<String> it = keys.iterator(); // Set에 접근하는 Iterator 리턴
+				while (it.hasNext()) {
+					String key = it.next(); // 키, 등록된 사용자의 이름.
 
-				boolean found = false;
-//				for (PersonLsy1205Test person : persons) {
-//					boolean result = person.getName().equals(search);
-//					if (result) {
-//						String result2 = "이름 : " + person.getName() + ", 연락처 : " + person.getContact() + ", 등록 날짜 : "
-//								+ person.getRegDate();
-//						System.out.println(result2);
-//						found = true;
-//					}
-//				}
-				if (!found) {
-					System.out.println("사용자가 없습니다. ");
+					if (key.equals(search)) {
+						PersonLsy1205Test value = persons.get(key); // 값
+						System.out.println("(이름: " + key + " , 연락처 : " + value.getContact() + ", 등록날짜 : "
+								+ value.getRegDate() + " ) ");
+					}
+
 				}
 
 			} else if (check.equals("4")) {
 				System.out.println("삭제 기능을 구현 할 예정");
 				System.out.println("삭제할 이름 작성:>>");
 				String userName = scanner.next();
-				// found , 있다면 삭제 진행, 없다면, 사용자가 없습니다. 
+				// found , 있다면 삭제 진행, 없다면, 사용자가 없습니다.
 				boolean found = false;
 				PersonLsy1205Test removePerson = null;
 				// 임시 저장소에 있는 모든 사용자 검색.
@@ -93,26 +94,26 @@ public class Lsy1205HashMapVersion {
 				} else {
 					System.out.println("사용자가 없습니다.");
 				}
-				
+
 			} else if (check.equals("5")) {
 				System.out.println("랜덤 기능을 구현 할 예정");
-				// 연락처에 사용자가 있다면, 
-				if(!persons.isEmpty()) {
+				// 연락처에 사용자가 있다면,
+				if (!persons.isEmpty()) {
 					Random random = new Random();
-					// 임시 메모리 공간에 등록된 크기만큼만해서, 3명 
-					// 0, 1,2 랜덤한 정수 선택하기. 
+					// 임시 메모리 공간에 등록된 크기만큼만해서, 3명
+					// 0, 1,2 랜덤한 정수 선택하기.
 					int randomIndexNumber = random.nextInt(persons.size());
 					System.out.println("선택된 랜덤 인덱스 숫자 확인: " + randomIndexNumber);
 					// 랜덤 인덱스 번호 이용해서, 해당 ArrayList 의 요소에 접근.
 					PersonLsy1205Test person = persons.get(randomIndexNumber);
-					String result = "랜덤으로 선택된 사용자, 이름 : " + person.getName() + ", 연락처 : " + person.getContact() + ", 등록 날짜 : "
-							+ person.getRegDate();
+					String result = "랜덤으로 선택된 사용자, 이름 : " + person.getName() + ", 연락처 : " + person.getContact()
+							+ ", 등록 날짜 : " + person.getRegDate();
 					System.out.println(result);
-					
+
 				} else {
 					System.out.println("연락처 등록된 사용자가 없습니다.");
 				}
-				
+
 			} else if (check.equals("6")) {
 				System.out.println("프로그램 종료");
 				scanner.close();
@@ -124,26 +125,19 @@ public class Lsy1205HashMapVersion {
 	}
 
 	public static void main(String[] args) {
-		// 임시 저장소 ArrayList -> HashMap 교체, 
-		// 차이점, 
+		// 임시 저장소 ArrayList -> HashMap 교체,
+		// 차이점,
 		// ArrayList: 순서(인덱스), 중복 허용. 입력, 이상용(0번 인덱스 저장)
 		// 입력, 이상용 (1번 인덱스 저장. )
 		// HashMap : 자료 구조 형태, 키 : 값으로 저장하는 구조.
-		// 기준 키를 기준으로 함. 중복을 허용을 안함. 
+		// 기준 키를 기준으로 함. 중복을 허용을 안함.
 		// 예) , 키 : 이상용, 값 : 123
-		// 예2), 키 : 이상용, 값 : 456, 기존 값에 덮어쓰기가 됩니다. 
-		
-		HashMap<String,PersonLsy1205Test> persons = new HashMap<String,PersonLsy1205Test>();
+		// 예2), 키 : 이상용, 값 : 456, 기존 값에 덮어쓰기가 됩니다.
+
+		HashMap<String, PersonLsy1205Test> persons = new HashMap<String, PersonLsy1205Test>();
 		Scanner scanner = new Scanner(System.in);
-	
+
 		contactProgram(scanner, persons);
 
 	} // main
 }
-
-
-
-
-
-
-
