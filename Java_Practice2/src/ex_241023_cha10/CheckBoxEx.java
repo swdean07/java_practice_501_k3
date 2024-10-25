c1946eebcbce901cf7d3748b57c7bfca51da3758
이상용 <lsy3709@naver.com>
2024-10-25 15:37:30 +0900

    6교시, 스윙 컴포넌트들 종류 확인 예제4. JCheckBox, 이벤트 리스너 예제

--- Java_Practice2/src/ex_241023_cha10/CheckBoxEx.java
+++ Java_Practice2/src/ex_241023_cha10/CheckBoxEx.java
@@ -3,6 +3,7 @@ package ex_241023_cha10;
 import java.awt.Container;
 import java.awt.FlowLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 
@@ -12,13 +13,20 @@ public class CheckBoxEx extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
+		
 		// 3개의 체크박스를 생성한다.
 		JCheckBox apple = new JCheckBox("사과");
 		JCheckBox pear = new JCheckBox("배", true);
 		JCheckBox cherry = new JCheckBox("체리");
+		ImageIcon normalIcon = new ImageIcon("images/test11.png");
+		JCheckBox test11 = new JCheckBox("test11",normalIcon,true);
+		
 		c.add(apple);
 		c.add(pear);
 		c.add(cherry);
+		
+		c.add(test11);
+		
 		setSize(250, 150);
 		setVisible(true);
 	}
