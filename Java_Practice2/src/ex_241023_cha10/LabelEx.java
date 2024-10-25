3d2f9df37de26df1890f5338db3e89a5d2840edb
이상용 <lsy3709@naver.com>
2024-10-25 15:27:35 +0900

    6교시, 스윙 컴포넌트들 종류 확인 예제3. JButton

--- Java_Practice2/src/ex_241023_cha10/LabelEx.java
+++ Java_Practice2/src/ex_241023_cha10/LabelEx.java
@@ -14,19 +14,24 @@ public class LabelEx extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
+		//문자열 라벨. 
 		JLabel textLabel = new JLabel("제임스 고슬링 입니더!");
+		c.add(textLabel);
+		
+		// ImageIcon 타입으로 이미지 폴더 경로에 있는 이미지를 선택.
 		ImageIcon img = new ImageIcon("images/test11.png");
 		
 		// 주의사항, 이미지 폴더 경로는, 자바프로젝트 바로 밑에 만들기. 
-		// 
-		
+		// 이미지 라벨.
 		JLabel imageLabel = new JLabel(img);
+		c.add(imageLabel);
+		
+		//3. 문자열 + 이미지 , 라벨
 		ImageIcon icon = new ImageIcon("images/test22.png");
 		JLabel label = new JLabel("커피한잔 하실래예, 전화주이소", icon, SwingConstants.CENTER);
-		c.add(imageLabel);
 		c.add(label);
 		
-		c.add(textLabel);
+		
 		setSize(300, 500);
 		setVisible(true);
 		
