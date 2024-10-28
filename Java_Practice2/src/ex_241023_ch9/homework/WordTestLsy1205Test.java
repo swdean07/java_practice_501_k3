f34322f0ef0590827ce64abb8800453c18c7f278
이상용 <lsy3709@naver.com>
2024-10-28 09:53:28 +0900

    1교시, 미니실습 풀이중, 이벤트 리스너 설정 및 맵에 저장.

--- Java_Practice2/src/ex_241023_ch9/homework/WordTestLsy1205Test.java
+++ Java_Practice2/src/ex_241023_ch9/homework/WordTestLsy1205Test.java
@@ -22,7 +22,7 @@ public class WordTestLsy1205Test extends JFrame {
 	// 전역으로 선언만 했음. 생성자에서 초기화 하면, 다른 곳에서 사용이 가능
 	private JTextField wordField; //  영어단어 입력 필드 
 	private JTextField meaningField; //  영어 뜻 입력 필드
-	private JPanel wordPanel; // 단어 표실할 필드 
+	private JPanel wordPanel; // 단어 표시할 필드 
 	
 	// 단어를 임시로 저장할 Map -> HashMap  
 	// UI, 화면에 표시할 라벨
@@ -77,11 +77,92 @@ public class WordTestLsy1205Test extends JFrame {
 		// 단어를 추가하는 버튼 
 		inputPanel.add(new JButton("단어추가"));
 		
+		// 랜덤으로 출력해주는 버튼 
+		JButton addButton = new JButton("랜덤 단어 표시");
+		inputPanel.add(addButton);
+		
+		//==================================================
+		
+		// 단어 출력 해주는 UI 
+		wordPanel = new JPanel();
+		// wordPanel 에서 배치 관리자, flex 처럼 나란히 배치 FlowLayout 이용. 
+		wordPanel.setLayout(new FlowLayout());
+		
+		// 최상위 프레임 창에 
 		// 단어를 입력하는 패널을, 북쪽에 붙이기 작업. 
 		add(inputPanel,BorderLayout.NORTH);
+		// 단어를 출력하는 패널을, 가운데 붙이기 작업. 
+		add(wordPanel,BorderLayout.CENTER);
+		
+		//==================================================
+		// 각 버튼에 이벤트 핸들러(리스너) 붙이기 작업. 
+		// addActionListener , 함수형 인터페이스 : 추상 메서드를 하나만 가짐. 
+		// 그래서, 람다식(화살표 함수) 표현 가능. 
+		// 이벤트 파라미터를 받아서, addWord() 메서드 이용해서, 추가하는 로직. 
+		// 맵에 추가할  예정. 
+		addButton.addActionListener(e -> addWord());
 		
 		setVisible(true);
 		
+		
+	}
+	
+	// Map 에 단어를 추가하는 로직. 
+	// 입력란에 입력된 내용을 추가하는 로직. 
+	private void addWord() {
+		// 입력된 단어 가져오기. 
+		// 전역으로 설정된, 단어 입력란 참조형 변수를 이용해서 가지고 올 예정.
+		//1
+		String word = wordField.getText();
+		
+		// 입력된 뜻 가져오기.
+		//2
+		String meaning = meaningField.getText();
+		
+		// 기본 유효성 체크, 단어와 뜻에 입력이 된 경우만 로직을 진행.
+		//3
+		if(!word.isEmpty() && !meaning.isEmpty()) {
+			// 임시로 출력할 JLabel를 만들어서, 여기에 단어를 붙이는 과정. 
+			// 임시 라벨 생성. 
+			JLabel wordLabel = new JLabel(word);
+			// 마우스 커서를 손 모양으로 변경하는 기능. 옵션, 
+			wordLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			
+			// 이벤트 리스너 추가하기. , 마우스 클릭시 뜻을 보여주는 기능.
+			// addMouseListener , 함수형 인터페이스가 아님. 간단히 추상 메서드가 2개이상. 
+			// 인터페이스 그냥 구현하면, 사용 안하는 추상메서드 모두를  일단 재정의
+			// 조금 더 간편히 하기 위해서, 어댑터 클래스 이용하면, 사용할 추상 메서드만,
+			// 이용 가능함. 
+			// 익명 클래스로 구현. 
+			// new 부모 클래스의(인터페이스, 클래스) {}
+			wordLabel.addMouseListener(new MouseAdapter() {
+				// 상태 변수, 설정, 뜻을 보여주는 상태. 
+				private boolean showMeaning = false;
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					if (!showMeaning) {
+						wordLabel.setText("뜻 : " + meaning);
+						showMeaning = true;
+					} else {
+						wordLabel.setText(word);
+						showMeaning = false;
+					}
+				}
+			});
+			// Map 메모리에 저장하는 로직. 
+			wordLabelMap.put(word, wordLabel);
+			wordMeaningLabelMap.put(word, meaning);
+			
+			// 패널에 라벨 추가. 
+			wordPanel.add(wordLabel);
+			// 레이아웃 업데이트 
+			wordPanel.revalidate();
+			
+		} // if
+		// 유효성 통과 안될 때, 경고창 보여주기. 
+		else {
+			
+		}
 		
 	}
 	
