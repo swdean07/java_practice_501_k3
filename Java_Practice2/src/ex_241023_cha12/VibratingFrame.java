9149a277d899c6692a3dc5ea2ddf092cd4cbdec6
이상용 <lsy3709@naver.com>
2024-10-28 15:41:22 +0900

    6교시, Thread , 타 스레드 강제 종료 예제5, 클릭 마다, 스레드 시작/종료 반복

--- Java_Practice2/src/ex_241023_cha12/VibratingFrame.java
+++ Java_Practice2/src/ex_241023_cha12/VibratingFrame.java
@@ -15,14 +15,21 @@ public class VibratingFrame extends JFrame implements Runnable {
 		setSize(200, 200);
 		setLocation(300, 300);
 		setVisible(true);
+		
+		// 프레임 내부에 기본 패널에, 마우스 클릭 리스너 설정. 
 		getContentPane().addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
-				if (!th.isAlive())
-					return;
+				if (th != null && th.isAlive()) {
 				th.interrupt();
+				} else {
+					th = new Thread(VibratingFrame.this);
+					th.start(); // 진동 시작
+				}
 			}
 		});
+		// 방법2, Thread 클래스 생성자에, 인터페이스 구현한 클래스의 인스턴스 주입. 
 		th = new Thread(this); // 진동하는 스레드 객체 생성
+		// 스레드 동작 시작.
 		th.start(); // 진동 시작
 	}
 
@@ -32,10 +39,12 @@ public class VibratingFrame extends JFrame implements Runnable {
 		Random r = new Random();
 		while (true) {
 			try {
+				// 20ms 대기
 				Thread.sleep(20); // 20ms 잠자기
 			} catch (InterruptedException e) {
 				return; // 리턴하면 스레드 종료
 			}
+			// r.nextInt() % 5 : -4 이상 , 4 이하, 랜덤한 정수를 뽑기
 			int x = getX() + r.nextInt() % 5; // 새 위치 x
 			int y = getY() + r.nextInt() % 5; // 새 위치 y
 
