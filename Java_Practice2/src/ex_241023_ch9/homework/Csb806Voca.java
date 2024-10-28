ccc6aff5f8dd7bf56d569a1dbdc18360a1a291ca
subin806 <chosb0806@naver.com>
2024-10-28 11:30:47 +0900

    조수빈 단어장 실습 수정

--- Java_Practice2/src/ex_241023_ch9/homework/Csb806Voca.java
+++ Java_Practice2/src/ex_241023_ch9/homework/Csb806Voca.java
@@ -7,68 +7,43 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Csb806Voca extends JFrame {
-    private JLabel a = new JLabel("apple");
-    private JLabel b = new JLabel("banana");
-    private JLabel c = new JLabel("house");
-    private JLabel d = new JLabel("car");
+    private JLabel[] labels = new JLabel[4];
+    private String[] englishWords = {"apple", "banana", "house", "car"};
+    private String[] koreanWords = {"사과", "바나나", "집", "차"};
 
     public Csb806Voca() {
         setTitle("Subin Voca");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container c = getContentPane();
-        c.setLayout(null);
+        Container container = getContentPane();
+        container.setLayout(null);
 
-
-        a.setSize(100, 20);
-        a.setLocation(200, 60);
-        b.setSize(100, 20);
-        b.setLocation(200, 80);
-        c.setSize(100, 20);
-        c.setLocation(200, 100);
-        d.setSize(100, 20);
-        d.setLocation(200, 120);
-
-
-        Mouse mouse = new Mouse();
-        a.addMouseListener(mouse);
-        b.addMouseListener(mouse);
-        c.addMouseListener(mouse);
-        d.addMouseListener(mouse);
-
-        c.add(a); 
-        c.add(b); 
-        c.add(c); 
-        c.add(d); 
+        for (int i = 0; i < labels.length; i++) {
+            labels[i] = new JLabel(englishWords[i]);
+            labels[i].setSize(100, 20);
+            labels[i].setLocation(200, 60 + i * 20);
+            labels[i].addMouseListener(new MouseEventHandler(i));
+            container.add(labels[i]);
+        }
 
         setSize(500, 300);
         setVisible(true);
     }
 
-    class Mouse extends MouseAdapter {
-        private String originalText;
+    private class MouseEventHandler extends MouseAdapter {
+        private final int index;
 
-        public void mousePressed(MouseEvent e) {
-            JLabel word = (JLabel) e.getSource();
-            originalText = word.getText(); 
-            switch (originalText) {
-                case "apple":
-                    word.setText("사과");
-                    break;
-                case "banana":
-                    word.setText("바나나");
-                    break;
-                case "house":
-                    word.setText("집");
-                    break;
-                case "car":
-                    word.setText("차");
-                    break;
-            }
+        public MouseEventHandler(int index) {
+            this.index = index;
         }
 
+        @Override
+        public void mousePressed(MouseEvent e) {
+            labels[index].setText(koreanWords[index]);
+        }
+
+        @Override
         public void mouseReleased(MouseEvent e) {
-            JLabel word = (JLabel) e.getSource();
-            word.setText(originalText);
+            labels[index].setText(englishWords[index]);
         }
     }
 
