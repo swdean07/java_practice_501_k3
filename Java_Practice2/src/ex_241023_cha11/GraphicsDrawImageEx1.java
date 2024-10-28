73d3d5530a0777af4ba6ba5124472afcf388c752
이상용 <lsy3709@naver.com>
2024-10-28 12:06:38 +0900

    4교시, 그래픽, 이미지 그리기 재정의 예제4

---
+++ Java_Practice2/src/ex_241023_cha11/GraphicsDrawImageEx1.java
@@ -0,0 +1,33 @@
+package ex_241023_cha11;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GraphicsDrawImageEx1 extends JFrame {
+	public GraphicsDrawImageEx1() {
+		setTitle("원본 크기로 원하는 위치에 이미지 그리기");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(new MyPanel());
+
+		setSize(300, 400);
+		setVisible(true);
+	}
+
+	class MyPanel extends JPanel {
+		private ImageIcon icon = new ImageIcon("images/test1.jpg");
+		private Image img = icon.getImage();
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(img, 20, 20, this);
+		}
+	}
+
+	public static void main(String[] args) {
+		new GraphicsDrawImageEx1();
+	}
+}
