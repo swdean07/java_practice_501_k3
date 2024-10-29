5b5a0cb073fcbf05d9f93cf2047c0757253a94a7
이상용 <lsy3709@naver.com>
2024-10-29 10:46:23 +0900

    2교시,파일입출력, File 클래스 이용해서, 파일 복사 예제3, 특정 배열 크기 만큼 바이트 읽기,쓰기, 이미지 복사 확인

--- Java_Practice2/src/ex_241023_cha13/BlockBinaryCopy.java
+++ Java_Practice2/src/ex_241023_cha13/BlockBinaryCopy.java
@@ -12,15 +12,22 @@ public class BlockBinaryCopy {
 		// 출력 대상 
 		File dest = new File("c:\\Temp\\copyTest1.jpg");
 		try {
+			// 원본 이미지 읽어서 -> FileInputStream 에 담기
 			FileInputStream fi = new FileInputStream(src); // 파일 입력 바이트 스트림 생성
+			// 복사본 이미지 파일 읽어서 -> FileOutputStream 에 담기, 출력 대상 이미지 파일. 
 			FileOutputStream fo = new FileOutputStream(dest); // 파일 출력 바이트 스트림 생성
 
-
+			// 임시 배열 만들기, 바이트 타입으로 
 			byte[] buf = new byte[1024 * 10]; // 10KB 버퍼
 			while (true) {
 				// 다른점, 읽을 때, 특정 배열의 크기만큼 씩 읽기
 				int n = fi.read(buf); // 버퍼 크기만큼 읽기. n은 실제 읽은 바이트
 				// 다른점, 쓰기 작업 할  때, 특정 배열의 크기만큼 씩 쓰기
+				// 
+				// buf, 임시 저장 공간
+				// fi , 읽어서 buf 만큼 씩 읽고, 
+				// fo, buf 만큼 씩 쓰기 작업을 진행함. 
+				
 				fo.write(buf, 0, n); // buf[0]부터 n 바이트 쓰기
 				if (n < buf.length)
 					break; // 버퍼 크기보다 작게 읽었기 때문에 파일 끝에 도달. 복사 종료
