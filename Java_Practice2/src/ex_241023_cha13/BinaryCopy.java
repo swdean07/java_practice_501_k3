1ab6ca415b90b9f95db938d2b7a5b36592ec6eed
이상용 <lsy3709@naver.com>
2024-10-29 10:39:12 +0900

    2교시,파일입출력, File 클래스 이용해서, 파일 복사 예제3, 특정 배열 크기 만큼 바이트 읽기,쓰기, 이미지 복사2

--- Java_Practice2/src/ex_241023_cha13/BinaryCopy.java
+++ Java_Practice2/src/ex_241023_cha13/BinaryCopy.java
@@ -7,15 +7,23 @@ import java.io.IOException;
 
 public class BinaryCopy {
 	public static void main(String[] args) {
+		// 입력 대상
 		File src = new File("c:\\Temp\\test1.jpg");
+		// 출력 대상 
 		File dest = new File("c:\\Temp\\copyTest1.jpg");
 		int c;
 		try {
+			// 입력 대상을 읽어서 -> FileInputStream 에 담기 (메모리) 
+			// -> 현재 파일 , 응용프로그램
 			FileInputStream fi = new FileInputStream(src);
+			// 출력 대상을 읽어서 -> FileOutputStream 에 담기
 			FileOutputStream fo = new FileOutputStream(dest);
+			// 1바이트 씩 읽어서
 			while ((c = fi.read()) != -1) {
+				// 1바이트 씩 출력하기.
 				fo.write((byte) c);
 			}
+			// 자원반납
 			fi.close();
 			fo.close();
 			System.out.println(src.getPath() + "를 " + dest.getPath() + "로 복사하였습니다.");
---
+++ Java_Practice2/src/ex_241023_cha13/BlockBinaryCopy.java
@@ -0,0 +1,35 @@
+package ex_241023_cha13;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BlockBinaryCopy {
+	public static void main(String[] args) {
+		// 입력 대상
+		File src = new File("c:\\Temp\\test1.jpg");
+		// 출력 대상 
+		File dest = new File("c:\\Temp\\copyTest1.jpg");
+		try {
+			FileInputStream fi = new FileInputStream(src); // 파일 입력 바이트 스트림 생성
+			FileOutputStream fo = new FileOutputStream(dest); // 파일 출력 바이트 스트림 생성
+
+
+			byte[] buf = new byte[1024 * 10]; // 10KB 버퍼
+			while (true) {
+				// 다른점, 읽을 때, 특정 배열의 크기만큼 씩 읽기
+				int n = fi.read(buf); // 버퍼 크기만큼 읽기. n은 실제 읽은 바이트
+				// 다른점, 쓰기 작업 할  때, 특정 배열의 크기만큼 씩 쓰기
+				fo.write(buf, 0, n); // buf[0]부터 n 바이트 쓰기
+				if (n < buf.length)
+					break; // 버퍼 크기보다 작게 읽었기 때문에 파일 끝에 도달. 복사 종료
+			}
+			fi.close();
+			fo.close();
+			System.out.println(src.getPath() + "를 " + dest.getPath() + "로 복사하였습니다.");
+		} catch (IOException e) {
+			System.out.println("파일 복사 오류");
+		}
+	}
+}
