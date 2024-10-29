cbd2a21e5d789746eb51ebdbf5dbc706d0cccff5
이상용 <lsy3709@naver.com>
2024-10-29 10:10:52 +0900

    2교시,파일입출력, 출력 대상 파일에서 바이트 읽기 작업 예제

---
+++ Java_Practice2/src/ex_241023_cha13/FileInputStreamEx.java
@@ -0,0 +1,26 @@
+package ex_241023_cha13;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileInputStreamEx {
+	public static void main(String[] args) {
+		byte b[] = new byte[6]; // 비어 있는 byte 배열
+		try {
+			FileInputStream fin = new FileInputStream("c:\\Temp\\test.out");
+			int n = 0, c;
+			while ((c = fin.read()) != -1) {
+				b[n] = (byte) c; // 읽은 바이트를 배열에 저장
+				n++;
+			}
+
+			System.out.println("c:\\Temp\\test.out에서 읽은 배열을 출력합니다.");
+			for (int i = 0; i < b.length; i++)
+				System.out.print(b[i] + " ");
+			System.out.println();
+
+			fin.close();
+		} catch (IOException e) {
+		}
+	}
+}
