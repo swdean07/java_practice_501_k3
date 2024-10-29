2f69e00c748483b05e9d22dcd742bfe88be16397
이상용 <lsy3709@naver.com>
2024-10-29 10:04:53 +0900

    2교시,파일입출력, 임시 배열의 값으로 바이트 쓰기 작업 예제

---
+++ Java_Practice2/src/ex_241023_cha13/FileOutputStreamEx.java
@@ -0,0 +1,16 @@
+package ex_241023_cha13;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamEx {
+	public static void main(String[] args) { 
+		byte b[] = {7,51,3,4,-1,24}; 
+		try { 
+		FileOutputStream fout = new FileOutputStream("c:\\Temp\\test.out"); 
+		for(int i=0; i<b.length; i++) 
+		fout.write(b[i]); // 배열 b의 바이너리를 그대로 기록 
+		fout.close(); 
+		} catch(IOException e) { } 
+		} 
+}
