7f37c3a2f434a42132ee2e9743b27bc106d738fd
이상용 <lsy3709@naver.com>
2024-10-30 15:29:03 +0900

    6교시, JDBC 드라이버 프로젝트 설정 준비물, 확인 파일2, 오타수정

--- Java_Practice2/src/ex_241023_cha15/JDBC_select_ex_1.java
+++ Java_Practice2/src/ex_241023_cha15/JDBC_select_ex_1.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class JDBC_select_ex_1 {
 	public static void main(String[] args) {
 		 String driver = "oracle.jdbc.driver.OracleDriver";   // 12행 ~ 15행 데이터베이스 접속을 위한 4가지 정보를 String  변수에  저장.
-		 String url = "jdbc:oracle:thin:localhost:xe";
+		 String url = "jdbc:oracle:thin:localhost:1521:xe";
 		 String userid = "scott";
 		 String passwd = "tiger";
 		 Connection con = null;
