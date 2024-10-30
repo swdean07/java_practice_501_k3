a1373b0cc2474f36a82ba3df3f1465904e82ca0a
이상용 <lsy3709@naver.com>
2024-10-30 09:23:57 +0900

    1030, 1교시 커밋 정리

--- Java_Practice2/src/ex_241023_cha14/multiChat/ClientBackground.java
+++ Java_Practice2/src/ex_241023_cha14/multiChat/ClientBackground.java
@@ -25,7 +25,8 @@ public class ClientBackground {
 	// 클라이언트 접속이 완료되면
 	public void connect() {
 		try {
-			socket = new Socket("10.100.104.27", 7777);
+			// 상대방 서버의 아이피 입력.
+			socket = new Socket("10.100.201.87", 7777);
 			System.out.println("서버에 연결됨");
 
 			out = new DataOutputStream(socket.getOutputStream());
