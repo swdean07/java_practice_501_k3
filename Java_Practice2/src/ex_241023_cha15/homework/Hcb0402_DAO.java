3f1fe1cbb3b3090939ad042c63e9bc4ec8d6d54e
Yuu-Yur <gkcjdqls@gmail.com>
2024-10-31 15:02:19 +0900

    하청빈 1031 3~4교시 실습 삭제, 수정 기능 추가

--- Java_Practice2/src/ex_241023_cha15/homework/Hcb0402_DAO.java
+++ Java_Practice2/src/ex_241023_cha15/homework/Hcb0402_DAO.java
@@ -3,12 +3,17 @@ package ex_241023_cha15.homework;
 import java.sql.*;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Hcb0402_DAO {
 	// property
 	String driver = "oracle.jdbc.driver.OracleDriver";
 	String url = "jdbc:oracle:thin:@localhost:1521:xe";
 	String userid = "scott";
 	String passwd = "tiger";
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
 
 	// constructor
 	public Hcb0402_DAO() {
@@ -22,9 +27,6 @@ public class Hcb0402_DAO {
 	// DTO list를 반환하는 select method 정의
 	public ArrayList<Hcb0402_DTO> select() {
 		ArrayList<Hcb0402_DTO> list = new ArrayList<Hcb0402_DTO>();
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 
 		try {
 			// 연결
@@ -57,34 +59,81 @@ public class Hcb0402_DAO {
 		}
 		return list;
 	}
-	
+
 	// DTO member을 받아 insert method 정의
 	public void insert(Hcb0402_DTO member) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 
 		try {
-			//연결
+			// 연결
 			con = DriverManager.getConnection(url, userid, passwd);
-			String query = "INSERT INTO member501(id,name,email,password)"+"VALUES(member501_seq.NEXTVAL,?,?,?)";
+			String query = "INSERT INTO member501(id,name,email,password)" + "VALUES(member501_seq.NEXTVAL,?,?,?)";
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, member.name);
 			pstmt.setString(2, member.email);
 			pstmt.setString(3, member.password);
 			// executeupdate로 insert문을 넘겨주고 변환된 행의 수를 int n 으로 받음
 			int n = pstmt.executeUpdate();
-			System.out.println(n+"개의 행이 추가되었습니다");
+			JOptionPane.showMessageDialog(null, n + "개의 행이 추가되었습니다", "추가 완료", JOptionPane.PLAIN_MESSAGE);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (rs != null)
-					rs.close();
 				if (pstmt != null)
 					pstmt.close();
 				if (con != null)
 					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// delete method 정의
+	public void delete(String name) {
+		try {
+			con = DriverManager.getConnection(url, userid, passwd);
+			String query = "DELETE FROM member501 WHERE name = ?";
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, name);
+			int n = pstmt.executeUpdate();
+			JOptionPane.showMessageDialog(null, n + "개의 행이 삭제되었습니다", "삭제 완료", JOptionPane.INFORMATION_MESSAGE);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// update method 정의
+	public void update(int id, String name, String email, String pass) {
+		try {
+			con = DriverManager.getConnection(url, userid, passwd);
+			String query = "UPDATE member501 SET name = ? , email = ? , password = ? WHERE id = ?";
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, name);
+			pstmt.setString(2, email);
+			pstmt.setString(3, pass);
+			pstmt.setInt(4, id);
+			int n = pstmt.executeUpdate();
+			JOptionPane.showMessageDialog(null, n + "개의 행이 수정되었습니다", "수정 완료", JOptionPane.PLAIN_MESSAGE);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+				// 사용한 자원을 finally 문을 이용해서 반납한다.
+				// 자원 반납은 사용했던 객체의 역순으로 하며 모두 공통적으로
+				// close() 메소드를 사용한다.
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
--- Java_Practice2/src/ex_241023_cha15/homework/Hcb0402_Frame.java
+++ Java_Practice2/src/ex_241023_cha15/homework/Hcb0402_Frame.java
@@ -1,21 +1,9 @@
 package ex_241023_cha15.homework;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.TextField;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.SwingConstants;
+import javax.swing.*;
 
 public class Hcb0402_Frame {
 	// component 선언
@@ -25,9 +13,15 @@ public class Hcb0402_Frame {
 	private static JPanel northPanel = new JPanel();
 	private static JButton cButton = new JButton("create");
 	private static JButton rButton = new JButton("read");
+	private static JButton dButton = new JButton("delete");
+	private static JButton uButton = new JButton("update");
 	private static TextField name = new TextField(10);
 	private static TextField email = new TextField(10);
 	private static TextField pass = new TextField(10);
+	private static int selectedId;
+	private static String selectedName;
+	private static String selectedEmail;
+	private static String selectedPassword;
 
 	// constructor
 	public Hcb0402_Frame() {
@@ -37,23 +31,25 @@ public class Hcb0402_Frame {
 		exFrame.setTitle("GridLayout 예제");
 		exFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		exFrame.setSize(800, 300);
-
-		// 추가 panel 생성, 저장된 정보 넣을 panel도 add, 왼쪽에 들어갈 jslidepane도 add
-
 		exFrame.setLayout(new BorderLayout());
+
+		// panel을 frame에 추가
 		exFrame.add(northPanel, BorderLayout.NORTH);
 		exFrame.add(centerPanel, BorderLayout.CENTER);
 		exFrame.add(westPanel, BorderLayout.WEST);
-		// jslidepane의 property
+
+		// west panel의 property
 		westPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		westPanel.setPreferredSize(new Dimension(200, 300));
-		// jpanel의 property
-		westPanelInside.setLayout(new BoxLayout(westPanelInside, BoxLayout.Y_AXIS));
-		// 출력 패널 레이아웃 세팅
 
-		// 추가 panel에 필요한 것들과 이벤트 리스너
+		// west panel inside의 property
+		westPanelInside.setLayout(new BoxLayout(westPanelInside, BoxLayout.Y_AXIS));
+
+		// northpanel에 들어갈 button의 이벤트 리스너
 		cButton.addActionListener(e -> create());
 		rButton.addActionListener(e -> read());
+		dButton.addActionListener(e -> delete());
+		uButton.addActionListener(e -> update());
 
 		// 추가 panel 에 add
 		northPanel.add(new JLabel("이름: "));
@@ -64,14 +60,18 @@ public class Hcb0402_Frame {
 		northPanel.add(pass);
 		northPanel.add(cButton);
 		northPanel.add(rButton);
+		northPanel.add(dButton);
+		northPanel.add(uButton);
 
 		// 끝
 		exFrame.setVisible(true);
 	}
-	
+
+	// method
 	// read method
 	private void read() {
 		westPanelInside.removeAll();
+		centerPanel.removeAll();
 		Hcb0402_DAO dao = new Hcb0402_DAO();
 		// JDBC_Ex_DAO 클래스를 사용하기 위해서 객체 생성.
 
@@ -85,38 +85,82 @@ public class Hcb0402_Frame {
 			String name = dto.getName();
 			String email = dto.getEmail();
 			String password = dto.getPassword();
-			JLabel idName = new JLabel(String.valueOf(id)+name);
+			JLabel idName = new JLabel(String.valueOf(id) + name);
 			idName.addMouseListener(new MouseAdapter() {
 				public void mouseClicked(MouseEvent e) {
 					centerPanel.removeAll();
-					JLabel centerLabel = new JLabel("이메일: "+email+" 비밀번호: "+password,SwingConstants.CENTER);
+					JLabel centerLabel = new JLabel("이메일: " + email + " 비밀번호: " + password, SwingConstants.CENTER);
 					centerLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
 					centerPanel.add(centerLabel);
+					selectedId = id;
+					selectedName = name;
+					selectedEmail = email;
+					selectedPassword = password;
 					centerPanel.revalidate();
 					centerPanel.repaint();
-					}
+				}
 			});
 			westPanelInside.add(idName);
 		}
 		westPanelInside.revalidate();
 		westPanelInside.repaint();
+		centerPanel.revalidate();
+		centerPanel.repaint();
 	}
 
 	// create method
 	private void create() {
-		if(!name.getText().isEmpty() && !email.getText().isEmpty() && !pass.getText().isEmpty()) {
-		Hcb0402_DTO member = new Hcb0402_DTO(name.getText(),email.getText(),pass.getText());
-		new Hcb0402_DAO().insert(member);
-		read();
+		if (!name.getText().isEmpty() && !email.getText().isEmpty() && !pass.getText().isEmpty()) {
+			Hcb0402_DTO member = new Hcb0402_DTO(name.getText(), email.getText(), pass.getText());
+			new Hcb0402_DAO().insert(member);
+			read();
 		} else {
 			JOptionPane.showMessageDialog(null, "이름과 이메일 비밀번호를 모두 입력해주세요.", "오류", JOptionPane.ERROR_MESSAGE);
 		}
-	}
-	
-	
-	
-	public static void main(String[] args) {
-		new Hcb0402_Frame();
-		}
+		name.setText("");
+		email.setText("");
+		pass.setText("");
 	}
 
+	// delete method
+private void delete() {
+		if (!name.getText().isEmpty()) {
+			new Hcb0402_DAO().delete(name.getText());
+			read();
+		} else {
+			JOptionPane.showMessageDialog(null, "삭제하고 싶은 이름을 입력해 주세요", "입력 오류", JOptionPane.ERROR_MESSAGE);
+		}
+		name.setText("");
+		email.setText("");
+		pass.setText("");
+	}
+
+	private void update() {
+		if (!name.getText().isEmpty() && !email.getText().isEmpty() && !pass.getText().isEmpty() && selectedId != 0) {
+			new Hcb0402_DAO().update(selectedId, name.getText(), email.getText(), pass.getText());
+			read();
+		} else if (!name.getText().isEmpty() && !email.getText().isEmpty()) {
+			new Hcb0402_DAO().update(selectedId, name.getText(), email.getText(), selectedPassword);
+		} else if (!name.getText().isEmpty() && !pass.getText().isEmpty()) {
+			new Hcb0402_DAO().update(selectedId, name.getText(), selectedEmail, pass.getText());
+		} else if (!email.getText().isEmpty() && !pass.getText().isEmpty()) {
+			new Hcb0402_DAO().update(selectedId, selectedName, email.getText(), pass.getText());
+		} else if (!name.getText().isEmpty()) {
+			new Hcb0402_DAO().update(selectedId, name.getText(), selectedEmail, selectedPassword);
+		} else if (!email.getText().isEmpty()) {
+			new Hcb0402_DAO().update(selectedId, selectedName, email.getText(), selectedPassword);
+		} else if (!pass.getText().isEmpty()) {
+			new Hcb0402_DAO().update(selectedId, selectedName, selectedEmail, pass.getText());
+		} else {
+			JOptionPane.showMessageDialog(null, "수정하려는 인물을 선택하고 이믈이나 이메일, 비밀번호를 입력해 주세요", "수정 오류", JOptionPane.ERROR_MESSAGE);
+		}
+		read();
+		name.setText("");
+		email.setText("");
+		pass.setText("");
+	}
+
+	public static void main(String[] args) {
+		new Hcb0402_Frame();
+	}
+}
